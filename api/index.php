abfd31873a80bdfc811d6625f3a83843ef7e933c
fillippo <alvarvaillo@gmail.com>
2025-07-01 12:37:15 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,11 +1,12 @@
 <?php
 //phpinfo();
-/*
-$content = file_get_contents('../public/index.html');
+
+$content = file_get_contents('/public/index.html');
 echo $content;
-*/
-$algo = dir("../*");
+
+$algo = dir("/*");
 echo $algo;
+*/
 /*  FUNCIONA:
 $db = new SQLite3('/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
